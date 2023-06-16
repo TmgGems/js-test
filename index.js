84125f69c74c8dd77f81cc249d535efcf54b045a
TmgGems <gyalwanglama123@gmail.com>
2023-06-16 14:10:39 +0545

    Operator added

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 var a = " ";
 consolelog(a);
+
+let x = 1;
+let y = 2;
+
+if(x == 1){
+    console.log("Yes it is 1  ");
+}
+
+if (x===1 && y ===2){
+    console.log("&&operator tested.")
+}
+
+if ((x=1) || (y=3)){
+    console.log("||Operator tested")
+}
