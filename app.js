e76f605d82120c5aa19488a14aae943a791b8af6
TmgGems <gyalwanglama123@gmail.com>
2023-06-21 13:56:03 +0545

    Module used calling another file

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const http = require("http");
+const http = require("http");// package or module name in parenthesis
+const date = require("./myfirstmodule");
 
  http.createServer((req,res) => {
     res.writeHead(200,{'Content-type':'text/html'});
-    res.end("Hello World");
- }).listen(8080);
+    res.end("Hello Boii" +  " Current time is : " + date.myDateTime());
+ })
+ .listen(8080);
 
  console.log('App Runing on port 8080');
---
+++ myfirstmodule.js
@@ -0,0 +1,6 @@
+exports.myDateTime = () =>{
+    //Module import garne vanne betekai export kwyword use garne
+    return Date();
+};
+
+//console.log(myDateTime())
