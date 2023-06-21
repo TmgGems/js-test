db154ee8df00527c779df9706a00609b4e7ac632
TmgGems <gyalwanglama123@gmail.com>
2023-06-21 14:48:42 +0545

    Reading contents of file

--- filemodule.js
+++ filemodule.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 
 const rs = fs.createReadStream('./file.txt');
 
-rs.on('open',function(){
+rs.on('open',() => {
     console.log("The file is open . ");
 });
+
+//to read the contents of file
+
+rs.on('data',(data) => {
+    console.log(data.toString());
+})
 
